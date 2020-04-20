ae1fe704cf12a0658cb813b0a8f8a4092d7f5dc1
Demir <demirmahmutovic@hotmail.com>
2020-04-20 14:17:14 +0200

    Added User section

---
+++ BulkyBook/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,78 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        #region API_CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+
+            var userList = _db.ApplicationUsers.Include(u => u.Company).ToList();
+            var userRole = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+
+            foreach (var user in userList)
+            {
+                var roleId = userRole.FirstOrDefault(u => u.UserId == user.Id).RoleId;
+                user.Role = roles.FirstOrDefault(u => u.Id == roleId).Name;
+                if (user.Company == null)
+                {
+                    user.Company = new Company()
+                    {
+                        Name = ""
+                    };
+                }
+
+            }
+
+            return Json(new { data = userList });
+        }
+
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody] string id)
+        {
+            var objFromDB = _db.ApplicationUsers.FirstOrDefault(u => u.Id == id);
+            if (objFromDB == null)
+            {
+                return Json(new { success = false, message = "Error while Locking/Unlocking" });
+            }
+            if(objFromDB.LockoutEnd!=null && objFromDB.LockoutEnd > DateTime.Now)
+            {
+                // user is currently locked
+                objFromDB.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                objFromDB.LockoutEnd = DateTime.Now.AddYears(1000);
+            }
+            _db.SaveChanges();
+            return Json(new { success = true, message = "Operation successfull" });
+        }
+
+        #endregion
+    }
+}
